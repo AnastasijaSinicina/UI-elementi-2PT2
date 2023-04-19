2f9d6dfbed70bde7d0ddd2f0364cecd8dcbec6d7
Anastasija <sinica.nastja@gmail.com>
2023-04-19 12:21:41 +0300

    Sagatavots viss pirma skripta rakstisanai

---
+++ Assets/Skripti/Varda_paradisana.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Varda_paradisana : MonoBehaviour {
+
+    // Use this for initialization
+    public int kadruSk = 0;
+    void Start () {
+		
+    Debug.Log("Uzsakta programma! Tu esi " + kadruSk + " kadra.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Kadrs " + kadruSk);
+	}
+}
